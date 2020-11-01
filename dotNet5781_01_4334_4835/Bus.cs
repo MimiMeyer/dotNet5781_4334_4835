b9d947314cd0cb72831b2cca2da996eeed00f823
רות חדוה כהן <ruth.co.264@gmail.com>
2020-11-01 21:17:49 +0200

    1/11 targil1

---
+++ dotNet5781_01_4334_4835/Bus.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_4334_4835
+{
+    public class Bus
+    {
+        private string licensePlate;
+        private DateTime start_Date;
+        private int km;
+        private const int fullTank = 1200;//max
+        public int Tank { get; set; }//current amount of fuel
+
+        public string License_Plate
+        {
+            get
+            {
+                return licensePlate;
+            }
+            private set
+            {
+                if ((start_Date.Year < 2018 && value.Length == 7) || (start_Date.Year >= 2018 && value.Length == 8))
+                {
+                    licensePlate = value;
+                }
+                else
+                {
+                    throw new Exception("licsence plate not valid");
+                }
+            }
+        }
+        public int Km
+        {
+            get { return km; }
+            set
+            {
+                if (value >= 0)
+                {
+                    km = value;
+                }
+            }
+        }
+        public void Gas(int gas)
+        {
+            Tank = fullTank;
+        }
+
+        public Bus()
+        {
+            Console.WriteLine("Enter starting date: ");
+
+            bool success = DateTime.TryParse(Console.ReadLine(), out start_Date);
+            if (!success)
+            {
+                throw new Exception("Invalid date!");
+            }
+            Console.WriteLine("Enter license plate number:");
+            License_Plate = Console.ReadLine();
+            
+            
+
+
+        }
+    }
+}
---
+++ dotNet5781_01_4334_4835/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_4334_4835
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            List<Bus> busses = new List<Bus>();//create a list for busses
+            Choices choice;
+            bool success;
+            RM_Busses(busses, out choice, out success);//make actions with it
+        }
+        private static void RM_Busses(List<Bus> busses, out Choices choice, out bool success)
+        {
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Choose an action");
+                    Console.WriteLine("ADD, PICK, GAS_MAINTENANCE, TOTAL, EXIT = -1");
+                    success = Enum.TryParse(Console.ReadLine(), out choice);
+                }
+                while (success == false);
+                switch(choice)
+                {
+                    case Choices.ADD:
+                        try 
+                        {
+                            busses.Add(new Bus());
+                                
+                        }
+                        catch(Exception exception) {
+                            Console.WriteLine(exception.Message);
+                        }
+                        /*foreach(Bus bus in busses)
+                        {
+                            Console.WriteLine(bus)\
+                        }*/
+                        break;
+                    case Choices.PICK:
+                        Console.WriteLine("enter a license number");
+                        string license = Console.ReadLine().Replace("-", String.Empty);
+                        Bus findBus = null;
+                        foreach (Bus bus in busses)
+                        {
+                            if (bus.License_Plate == license)
+                            {
+                                findBus = bus;//bus is found 
+                                break;
+                            }
+                        }
+                        if (findBus != null)
+                        {
+                            Console.WriteLine(findBus);
+                        }
+                        Random ridingKm=new Random(DateTime.Now.Millisecond);
+                    
+                        break;
+                    case Choices.GAS_MAINTENANCE:
+                        break;
+                    case Choices.TOTAL:
+                        break;
+                    case Choices.EXIT:
+                        break;
+                }
+            } while (choice != Choices.EXIT);
+        }
+    }
+
+}
