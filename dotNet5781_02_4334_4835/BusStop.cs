c7fc098531072102378b96e65a7c0f2bc7f32232
MimiMeyer <mimiliebermanrox@gmail.com>
2020-11-07 22:26:17 +0200

    targil2 07/11

---
+++ dotNet5781_02_4334_4835/BusStop.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace dotNet5781_02_4334_4835
+{
+    class BusStop
+    {
+        private int stationCode;
+        private double latitude;
+        private double longitude;
+        public int BusStationKey
+        {
+            get => stationCode;
+            set
+            {
+                if (value > 0 && value < 1000000)
+                {
+                    stationCode = value;
+                }
+                else throw new Exception("input not valid");
+            }
+        }
+        public double Latitude
+        {
+            get => latitude;
+            set
+            {
+                if (value >= -90 && value <= 90)
+                {
+                    latitude = value;
+                }
+                else throw new Exception("value must be between -90 to 90");
+            }
+        }
+        public double Longitude
+        {
+            get => longitude;
+            set
+            {
+                if (value >= -180 && value <= 180)
+                {
+                    latitude = value;
+                }
+                else throw new Exception("value must be between -180 to 180");
+            }
+        }
+        public String Address
+        { get; set; }
+        public override string ToString()
+        {
+            return String.Format("Bus Station Code: {0}, {1}°N {2}°E", BusStationKey, Latitude, Longitude);
+        }
+    }
+}
---
+++ dotNet5781_02_4334_4835/BusStopLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_4334_4835
+{
+    class BusStopLine:BusStop
+    {
+        public double Distance { get; set; }
+        public TimeSpan TravelTime { get; set; }
+    }
+}
